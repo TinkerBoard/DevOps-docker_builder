abe2d9b4e2bf7c7ac0edfce8fa820523c7d4fb73
Leslie Yu <leslie_yu@asus.com>
2022-08-29 17:19:25 +0800

    Sync with linux4.4-rk3288-tinker_board
    Change-Id: I45d1e90ce4fdb72b50a378822cdf55f27d7d954a

--- Dockerfile
+++ Dockerfile
@@ -1,35 +1,14 @@
-FROM ubuntu:16.04
+FROM ubuntu:20.04
 
 ARG DEBIAN_FRONTEND=noninteractive
-ARG userid
-ARG groupid
-ARG username
 
-COPY packages /packages
-
-# Install required packages for building Tinker Edge R Debian
 # kmod: depmod is required by "make modules_install"
 RUN apt-get update && \
-    apt-get install -y make gcc bc python libssl-dev liblz4-tool sudo time \
-    g++ patch wget cpio unzip rsync bzip2 perl gcc-multilib git kmod parted \
-    gdisk udev expect gawk zip
+    apt-get install -y bc build-essential cpio device-tree-compiler dosfstools \
+    expect gawk git kmod libssl-dev parted python python3 qemu-user-static \
+    rsync sudo time tzdata udev wget zip
 
-# Install required package for building Tinker Edge R base Debian system
-RUN  apt-get update && \
-     apt-get install -y binfmt-support qemu-user-static live-build
-RUN dpkg -i /packages/* || apt-get install -f -y
-
-RUN apt-get install -y fakeroot
-
-RUN groupadd -g $groupid $username && \
-    useradd -m -u $userid -g $groupid $username && \
-    echo "$username ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-
-ENV HOME=/home/$username
-ENV USER=$username
-WORKDIR /source
-
-COPY entrypoint.sh /entrypoint.sh
-RUN chmod a+x /entrypoint.sh
-
-ENTRYPOINT ["/entrypoint.sh"]
+COPY packages .
+RUN dpkg -i debootstrap_1.0.87_all.deb live-build_3.0.5-1linaro1_all.deb
+RUN apt-get install -f -y
+RUN rm debootstrap_1.0.87_all.deb live-build_3.0.5-1linaro1_all.deb
--- docker-builder-run.sh
+++ docker-builder-run.sh
@@ -1,86 +1,14 @@
 #!/bin/bash
+#
+# Provide the Docker environment for ASUS IoT.
 
-set -xe
+export ASUS_DOCKER_ENV_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-start_time=$(date +"%s")
+source ${ASUS_DOCKER_ENV_DIR}/docker-builder-base.sh
 
-if [ -x "$(command -v docker)" ]; then
-    echo "Docker is installed and the execute permission is granted."
-    if getent group docker | grep &>/dev/null "\b$(id -un)\b"; then
-	echo "User $(id -un) is in the group docker."
-    else
-        echo "Docker is not managed as a non-root user."
-	echo "Please refer to the following URL to manage Docker as a non-root user."
-        echo "https://docs.docker.com/install/linux/linux-postinstall/"
-	exit
-    fi
-else
-    echo "Docker is not installed or the execute permission is not granted."
-    echo "Please refer to the following URL to install Docker."
-    echo "http://redmine.corpnet.asus/projects/configuration-management-service/wiki/Docker"
-    exit
-fi
+start_time=$SECONDS
 
-if dpkg-query -s qemu-user-static 1>/dev/null 2>&1; then
-    echo "The package qemu-user-static is installed."
-else
-    echo "The package qemu-user-static is not installed yet and it will be installed now."
-    sudo apt-get install -y qemu-user-static
-fi
+asus_docker_env_main "$@"
 
-if dpkg-query -s binfmt-support 1>/dev/null 2>&1; then
-    echo "The package binfmt-support is installed."
-else
-    echo "The package binfmt-support is not installed yet and it will be installed now."
-    sudo apt-get install -y binfmt-support
-fi
-
-DIRECTORY_PATH_TO_DOCKER_BUILDER="$(dirname $(readlink -f $0))"
-echo "DIRECTORY_PATH_TO_DOCKER_BUILDER: $DIRECTORY_PATH_TO_DOCKER_BUILDER"
-
-DIRECTORY_PATH_TO_SOURCE="$(dirname $DIRECTORY_PATH_TO_DOCKER_BUILDER)"
-
-if [ $# -eq 0 ]; then
-    echo "There is no directory path to the source provided."
-    echo "Use the default directory path to the source [$DIRECTORY_PATH_TO_SOURCE]."
-else
-    DIRECTORY_PATH_TO_SOURCE=$1
-    if [ ! -d $DIRECTORY_PATH_TO_SOURCE ]; then
-        echo "The source directory [$DIRECTORY_PATH_TO_SOURCE] is not found."
-        exit
-    fi
-fi
-
-DOCKER_IMAGE="asus/builder-tinker_edge_r-debian:latest"
-cp -r $DIRECTORY_PATH_TO_SOURCE/debian/ubuntu-build-service/packages $DIRECTORY_PATH_TO_SOURCE/docker_builder/.
-docker build --build-arg userid=$(id -u) --build-arg groupid=$(id -g) --build-arg username=$(id -un) -t $DOCKER_IMAGE \
-    --file $DIRECTORY_PATH_TO_DOCKER_BUILDER/Dockerfile $DIRECTORY_PATH_TO_DOCKER_BUILDER
-rm -rf $DIRECTORY_PATH_TO_SOURCE/docker_builder/packages
-
-OPTIONS="--interactive --privileged --rm --tty"
-OPTIONS+=" --volume $DIRECTORY_PATH_TO_SOURCE:/source"
-echo "Options to run docker: $OPTIONS"
-
-if [ $VERSION ] || [ $VERSION_NUMBER ]; then
-	docker run $OPTIONS $DOCKER_IMAGE chroot --skip-chdir --userspec=$USER:$USER / /bin/bash -c "VERSION=$VERSION VERSION_NUMBER=$VERSION_NUMBER ./build.sh"
-else
-	docker run $OPTIONS $DOCKER_IMAGE chroot --skip-chdir --userspec=$USER:$USER / /bin/bash -i
-fi
-
-end_time=$(date +"%s")
-tdiff=$(($end_time-$start_time))
-hours=$(($tdiff / 3600 ))
-mins=$((($tdiff % 3600) / 60))
-secs=$(($tdiff % 60))
-
-set +x
-
-echo -n "#### build completed "
-if [ $hours -gt 0 ] ; then
-	printf "(%02g:%02g:%02g (hh:mm:ss))" $hours $mins $secs
-elif [ $mins -gt 0 ] ; then
-	printf "(%02g:%02g (mm:ss))" $mins $secs
-elif [ $secs -gt 0 ] ; then
-	printf "(%s seconds)" $secs
-fi
-echo " ####"
+elapsed=$(( SECONDS - start_time ))
+eval "echo Elapsed time: $(date -ud "@$elapsed" +'$((%s/3600/24)) days %H hr %M mins %S secs')"
